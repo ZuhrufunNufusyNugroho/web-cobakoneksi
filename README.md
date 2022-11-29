358041bc179629e8d99ce471e870576ba91ee666
ZuhrufunNufusyNugroho <78089869+ZuhrufunNufusyNugroho@users.noreply.github.com>
2022-11-30 05:03:40 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# AWSweb
+# TAwebAWS
---
+++ koneksi.php
@@ -0,0 +1,26 @@
+<?php
+
+//include "parser-php-version.php";
+$hostname  = "localhost";
+$username  = "admin";
+$password  = "Alohomora00_";
+$dbname  = "batiku";
+$db = mysqli_connect($hostname, $username, $password) or die ('Koneksi Gagal! ');
+mysqli_connect($dbname);
+
+ //$Open = mysql_connect("localhost","root","");
+ //if (!$Open){
+ //die ("Koneksi ke Engine MySQL Gagal !<br>");
+ //}else{
+ //print ("Engine Connected<br>");
+ //}
+//$Koneksi = mysql_select_db("level");
+ //if (!$Koneksi){
+ //die ("Koneksi ke Database Gagal !");
+ //}
+ //else{
+ //print ("Database Connected<br><br><br>");
+ //}
+
+
+?>
